21011cfb0b9b8470ef02a9d9ed431b8970ca6635
K-ash-ish <57871796+K-ash-ish@users.noreply.github.com>
2022-03-13 15:48:53 +0530

    Update Service1Words.java

--- src/Service1Words.java
+++ src/Service1Words.java
@@ -1,19 +1,24 @@
 package com.assignment2.service1;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service1Words {
-    private java.lang.String word;
+    private List<String> word = new ArrayList<String>();
 
     public Service1Words() {}
 
-    public Service1Words(java.lang.String word) {
+    public Service1Words(List<String> word) {
         this.word = word;
     }
 
-    public java.lang.String getWord() {
+    public List<String> getWords() {
         return word;
     }
 
-    public void setWord(java.lang.String word) {
+    public void addWord(List<String> word) {
         this.word = word;
     }
 }
